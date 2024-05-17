1353235b6a4fb07ba369d2e1c2e86df481621127
FaluPeppe <larspetermoller@gmail.com>
2024-05-17 23:14:01 +0200

    Buggfix i skapa hamta data-skriptet, kopplat till åldersvariabler.

--- func_API.R
+++ func_API.R
@@ -1089,8 +1089,9 @@ skapa_hamta_data_skript_pxweb <- function(skickad_url_pxweb = NA,
           first()) {
         
           # variabler som går att eliminera (dvs. inte ha med i uttaget)
+        if (!(str_detect(tolower(var_kod), "alder|ålder") & !alder_ar_klartext) | str_detect(tolower(var_kod), "grupp")){
           paste0("  ", tolower(var_kod) %>% str_replace_all(" ", "_"), '_vekt <- if (!all(is.na(', tolower(var_kod), '_klartext))) hamta_kod_med_klartext(px_meta, ', tolower(var_kod) %>% str_replace_all(" ", "_"), '_klartext, skickad_fran_variabel = "', tolower(var_kod), '") else NA\n')
-          
+        } else NA
       } else {    # variabler som inte går att eliminera (göra uttag utan) men som är klartext till kod
         #if (!str_detect(tolower(var_kod), "alder|ålder") | str_detect(tolower(var_kod), "grupp")) paste0("  ", tolower(var_kod) %>% str_replace_all(" ", "_"), '_vekt <- hamta_kod_med_klartext(px_meta, ', tolower(var_kod) %>% str_replace_all(" ", "_"), '_klartext, skickad_fran_variabel = "', tolower(var_kod), '")\n')
         if (!(str_detect(tolower(var_kod), "alder|ålder") & !alder_ar_klartext) | str_detect(tolower(var_kod), "grupp")) paste0("  ", tolower(var_kod) %>% str_replace_all(" ", "_"), '_vekt <- hamta_kod_med_klartext(px_meta, ', tolower(var_kod) %>% str_replace_all(" ", "_"), '_klartext, skickad_fran_variabel = "', tolower(var_kod), '")\n')
